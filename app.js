128ebae5b4d59669c7d647baff27901789fbe445
77iu <112532158+77iu@users.noreply.github.com>
2023-01-07 08:37:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,326 @@
+const menu = document.getElementById('menu');
+const indicador = document.getElementById('indicador');
+const secciones = document.querySelectorAll('.seccion');
+
+let tamañoIndicador = menu.querySelector('a').offsetWidth;
+indicador.style.width = tamañoIndicador + 'px';
+
+let indexSeccionActiva;
+
+// Observer
+const observer = new IntersectionObserver((entradas, observer) => {
+	entradas.forEach(entrada => {
+		if(entrada.isIntersecting){
+			// Obtenemos cual es la seccion que esta entrando en pantalla.
+			// console.log(`La entrada ${entrada.target.id} esta intersectando`);
+
+			// Creamos un arreglo con las secciones y luego obtenemos el index del la seccion que esta en pantalla.
+			indexSeccionActiva = [...secciones].indexOf(entrada.target);
+			indicador.style.transform = `translateX(${tamañoIndicador * indexSeccionActiva}px)`;
+		}
+	});
+}, {
+	rootMargin: '-80px 0px 0px 0px',
+	threshold: 0.2
+});
+
+// Agregamos un observador para el hero.
+observer.observe(document.getElementById('hero'));
+
+// Asignamos un observador a cada una de las secciones
+secciones.forEach(seccion => observer.observe(seccion));
+
+// Evento para cuando la pantalla cambie de tamaño.
+const onResize = () => {
+	// Calculamos el nuevo tamaño que deberia tener el indicador.
+	tamañoIndicador = menu.querySelector('a').offsetWidth;
+
+	// Cambiamos el tamaño del indicador.
+	indicador.style.width = `${tamañoIndicador}px`;
+
+	// Volvemos a posicionar el indicador.
+	indicador.style.transform = `translateX(${tamañoIndicador * indexSeccionActiva}px)`;
+}
+
+window.addEventListener('resize', onResize);
+
+
+var z = document.getElementById("z");
+var show01 = document.getElementById("show01");
+var show02 = document.getElementById("show02");
+var show03 = document.getElementById("show03");
+var show04 = document.getElementById("show04");
+var show05 = document.getElementById("show05");
+var show06 = document.getElementById("show06");
+var show07 = document.getElementById("show07");
+var show08 = document.getElementById("show08");
+var show09 = document.getElementById("show09");
+var show10 = document.getElementById("show10");
+var show11 = document.getElementById("show11");
+var show12 = document.getElementById("show12");
+var show13 = document.getElementById("show13");
+var show14 = document.getElementById("show14");
+var show15 = document.getElementById("show15");
+var show16 = document.getElementById("show16");
+var show17 = document.getElementById("show17");
+var show18 = document.getElementById("show18");
+var show19 = document.getElementById("show19");
+var show20 = document.getElementById("show20");
+var show21 = document.getElementById("show21");
+var show22 = document.getElementById("show22");
+var show23 = document.getElementById("show23");
+var show24 = document.getElementById("show24");
+var show25 = document.getElementById("show25");
+var show26 = document.getElementById("show26");
+var show27 = document.getElementById("show27");
+var show28 = document.getElementById("show28");
+var show29 = document.getElementById("show29");
+var show30 = document.getElementById("show30");
+var show31 = document.getElementById("show31");
+var show32 = document.getElementById("show32");
+var show33 = document.getElementById("show33");
+var show34 = document.getElementById("show34");
+var show35 = document.getElementById("show35");
+var show36 = document.getElementById("show36");
+var show37 = document.getElementById("show37");
+var show38 = document.getElementById("show38");
+var show39 = document.getElementById("show39");
+var show40 = document.getElementById("show40");
+var show41 = document.getElementById("show41");
+var show42 = document.getElementById("show42");
+var show43 = document.getElementById("show43");
+var show44 = document.getElementById("show44");
+var modal = document.getElementById("myImg");
+var detalle = document.getElementById("caption");
+
+show01.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+	
+                                    }
+show02.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+ }
+ show03.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show04.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show05.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show06.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show07.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show08.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show09.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show10.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+	
+									}
+show11.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+ }
+ show12.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show13.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show14.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show15.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show16.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show17.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show18.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show19.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+	
+									}
+show20.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+ }
+ show21.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show22.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show23.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show24.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show25.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show26.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show27.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show28.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+	
+									}
+show29.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+ }
+ show30.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show31.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show32.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show33.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show34.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show35.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show36.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show37.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show38.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+	
+									}
+show39.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+ }
+ show40.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show41.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show42.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show43.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+show44.onclick = function(){
+	z.style.display ="block";
+	modal.src = this.src;
+	detalle.innerHTML = this.alt
+									}
+								
+		var span = document.getElementsByClassName("close")[0];
+        var span =document.getElementById("myImg");
+        span.onclick = function(){
+            z.style.display ="none";
+        }
